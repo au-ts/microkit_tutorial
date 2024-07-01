8ba82baa266ff6c67641b9dd2868fa0f83c6c6e2
Ivan Velickovic <i.velickovic@unsw.edu.au>
2024-07-01 23:02:45 +1000

    Minor website cleanup
    Signed-off-by: Ivan Velickovic <i.velickovic@unsw.edu.au>

--- website/develop.sh
+++ website/develop.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-export LEARNMICROKIT_WORD_URL='trustworthy.systems/projects/microkit/tutorial/word'
-export LEARNMICROKIT_DOWNLOADS_URL='trustworthy.systems/Downloads/microkit_tutorial'
+export TUTORIAL_WORD_URL='trustworthy.systems/projects/microkit/tutorial/word'
+export TUTORIAL_DOWNLOADS_URL='trustworthy.systems/Downloads/microkit_tutorial'
 
 mdbook serve --open
 
