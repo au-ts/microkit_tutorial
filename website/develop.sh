23ceeebddfc83c92cb9131d3cdf5d86200b3d49f
Ivan-Velickovic <i.velickovic@unsw.edu.au>
2025-03-22 12:01:36 +1100

    Update to Microkit 2.0.1
    Signed-off-by: Ivan-Velickovic <i.velickovic@unsw.edu.au>

--- website/develop.sh
+++ website/develop.sh
@@ -2,7 +2,7 @@
 
 export TUTORIAL_WORD_URL='trustworthy.systems/projects/microkit/tutorial/word'
 export TUTORIAL_DOWNLOADS_URL='trustworthy.systems/Downloads/microkit_tutorial'
-export MICROKIT_VERSION='2.0.0'
+export MICROKIT_VERSION='2.0.1'
 
 mdbook serve --open
 
